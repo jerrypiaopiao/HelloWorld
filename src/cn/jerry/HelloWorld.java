b44845786deda24cefff9b1d151fb803e05bf723
zhang_r <zhang_r@chinaxuhu.com>
2017-03-30 18:14:36 +0800

    change say world.

--- src/cn/jerry/HelloWorld.java
+++ src/cn/jerry/HelloWorld.java
@@ -26,7 +26,7 @@ package cn.jerry;
 public class HelloWorld {
 	
 	public static void main (String args[]) {
-		System.out.println("jenkins, Hello World");
+		System.out.println("hello jenkins");
 	}
 }
 
